bf5ae3336f0d6a2e08100c95f8761605092eeed8
Alston <0508alstonfernandes@gmail.com>
2025-03-14 11:23:44 +0530

    Update lineage makefile

--- lineage_doha.mk
+++ lineage_doha.mk
@@ -18,26 +18,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-# TODO: this?
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# TODO: Add this file
-# Inherit from doha device
+#TODO:Add File
+# Inherit from ginkgo device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_doha
 PRODUCT_DEVICE := doha
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_MODEL := Moto G8 Plus
+#TODO: this?
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
+TARGET_VENDOR_PRODUCT_NAME := doha
 
- PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="doha_n-user 10 QPIS30.28-Q3-28-26-4-1-7 f0f57 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="doha_n-user 10 QPIS30.28-Q3-28-26-4-1-7 f0f57 release-keys "
 
- BUILD_FINGERPRINT := motorola/doha_retail/doha:10/QPIS30.28-Q3-28-26-4-1-7/f0f57:user/release-keys
+BUILD_FINGERPRINT := motorola/doha_n/doha_n:10/QPIS30.28-Q3-28-26-4-1-7/f0f57:user/release-keys
